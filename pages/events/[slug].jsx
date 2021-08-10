96e6a62e609cd365d4e6fefa4f4b661503120dcc
Dimitri Obin <dimitriobin@gmail.com>
2021-08-10 23:04:38 +0200

    test commit

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -7,6 +7,12 @@ export default function EventPage() {
     return (
         <div className={styles.container}>
             <h1>My event: {router.query.slug}</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                molestiae consequatur nostrum corrupti veritatis dolor culpa
+                debitis vel in expedita minima, reiciendis dolore quae
+                similique.
+            </p>
         </div>
     );
 }
