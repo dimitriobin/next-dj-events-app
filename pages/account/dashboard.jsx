2f5903552b5fa8c17fa85bf4f31b05e502b5b003
Dimitri Obin <dimitriobin@gmail.com>
2021-08-24 20:27:32 +0200

    fetch custom events

--- pages/account/dashboard.jsx
+++ pages/account/dashboard.jsx
@@ -1,9 +1,29 @@
+import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+    console.log(events);
     return (
         <Layout title='User Dashboard'>
             <h1>Dashboard</h1>
         </Layout>
     );
+}
+
+export async function getServerSideProps({ req }) {
+    const { token } = parseCookies(req);
+
+    const res = await fetch(`${API_URL}/events/me`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    const events = await res.json();
+
+    return {
+        props: { events },
+    };
 }
